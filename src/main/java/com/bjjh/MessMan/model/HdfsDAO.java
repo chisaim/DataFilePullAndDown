26f67795df0b3b7949a51116d58fb0c5842c6301
ChiSaiM <944125621@qq.com>
2018-02-05 18:08:21 +0800

    add Commit

---
+++ src/main/java/com/bjjh/MessMan/model/HdfsDAO.java
@@ -0,0 +1,92 @@
+package com.bjjh.MessMan.model;
+
+
+import com.bjjh.MessMan.config.GetConfigMess;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class HdfsDAO {
+
+    private Logger logger = Logger.getLogger(HdfsDAO.class);
+
+    GetConfigMess getConfigMess = new GetConfigMess();
+
+    public FileSystem getHadoopFileSystem(){
+
+        FileSystem fs = null;
+        try {
+            fs = FileSystem.get(URI.create(getConfigMess.getHDFSURI()),new Configuration(),getConfigMess.getHdfsUserName());
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error(e);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            logger.error(e);
+        }
+
+        return fs;
+    }
+
+    public boolean createPath(FileSystem fs,String uri){
+
+        boolean b = false;
+        Path path = new Path(uri);
+
+        try {
+            b = fs.mkdirs(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error(e);
+        }
+
+        return b;
+    }
+
+    /**
+     * 从本地上传文件到HDFS
+     * @param fs
+     * @param HDFSPath
+     * @param localPath
+     */
+    public void uploadFileFromHDFS(FileSystem fs,String HDFSPath,String localPath){
+
+        Path fsPath = new Path(HDFSPath);
+        Path loPath = new Path(localPath);
+
+        try {
+            fs.copyFromLocalFile(loPath,fsPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error(e);
+        }
+
+    }
+
+    /**
+     * 从HDFS中下载文件到本地
+     * @param fs
+     * @param HDFSPath
+     * @param localPath
+     */
+    public void downloadFileFromHDFS(FileSystem fs,String HDFSPath,String localPath){
+
+        Path fsPath = new Path(HDFSPath);
+        Path loPath = new Path(localPath);
+
+        try {
+            fs.copyToLocalFile(fsPath,loPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error(e);
+        }
+
+    }
+
+
+
+}
